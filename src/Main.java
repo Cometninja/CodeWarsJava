cd22d8b712af16cb7b48ac3b7d105d03e4d2578b
Daniel Casey <daniel_mn_casey@hotmail.com>
2023-06-15 16:00:00 +0100

    added in more functions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println(repeatStr(5,"o"));
+
+        List<List<Integer>> sets = createTwoSetsOfEqualSum(844035);
+        int firstset = 0;
+        int secondset = 0;
+        for (int i :sets.get(0)) {
+            firstset += i;
+        }
+        for (int i : sets.get(1)){
+            secondset += i;
+        }
+        System.out.println(firstset +" "+ secondset);
     }
+
+    public static List<List<Integer>> createTwoSetsOfEqualSum(int n) {
+
+        final List<List<Integer>> sets = new ArrayList<>();
+        sets.add(new ArrayList<Integer>());
+        sets.add(new ArrayList<Integer>());
+        long number = 0;
+        for(int i = 1; i<=n;i++){
+            number += i;
+        }
+        System.out.println(number);
+
+        if (number % 2 == 0) {
+            number /= 2;
+            System.out.println(number);
+            for (int i = n; i > 0; i--) {
+                if (number - i >= 0) {
+                    sets.get(0).add(i);
+                    number -= i;
+                } else sets.get(1).add(i);
+            }
+        }
+        return sets;
+    }
+
     public static int squareSum(int[] n)
     {
         int number = 0;
